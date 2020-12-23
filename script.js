9ff1cf2db910cfea6267e6b7111428c4b3b79b46
user <user@users-MacBook-air.local>
2020-12-23 21:42:52 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    //if there is no guess
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      document.querySelector('.score').textContent = score;
+      score = score - 1;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
